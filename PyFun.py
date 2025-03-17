cfa02b0855a8daa33844b11e1de236a178a57ad2
unknown <virgileku2020@gmail.com>
2025-03-17 19:45:26 +0200

    version 3

--- PyFun.py
+++ PyFun.py
@@ -2,7 +2,14 @@
 # a = input("Enter the text you want : ")
 # print( len(a) )
 
-a = input("Enter the message :")
-a = a.replace(" ", "")
-print(a)
-print(len(a))
+# a = input("Enter the message :")
+# a = a.replace(" ", "")
+# print(a)
+# print(len(a))
+ABC = []
+for i in range(5):
+    a = input(f"Enter the string you want {i + 1}: ")
+    ABC.append(a.replace(" ",""))
+
+for i, j in enumerate(ABC):
+    print(f"The number of characters in string {i + 1} is {len(j)}")
